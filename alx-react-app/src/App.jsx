5be93ac37c45c2066175a82eff5ceceef2349c3e
sertse-bit <serhamre@gmail.com>
2025-09-12 17:11:53 +0300

    Add UserProfile component and update App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -6,7 +6,7 @@ import WelcomeMessage from './components/WelcomeMessage';
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
-
+import UserProfile from './components/UserProfile';
 function App() {
   const [count, setCount] = useState(0)
 
@@ -18,6 +18,19 @@ function App() {
       <div>
         <Header />
       <MainContent />
+      <div>
+  <UserProfile 
+    name="Alice" 
+    age={25} 
+    bio="Loves hiking and photography" 
+  />
+  <UserProfile 
+    name="Bob" 
+    age={30} 
+    bio="Enjoys cooking and traveling" 
+  />
+</div>
+
       <Footer />
       </div>
       <div>
